eb9aae39a780df69f6eca2b68ac512dc15328af4
yuvraj66 <srijald15@gmail.com>
2022-03-14 11:55:33 +0545

    db connection fixing

--- src/utils/service.js
+++ src/utils/service.js
@@ -3,7 +3,6 @@ import Api from "./api";
 
 const ServiceApi = axios.create({
   baseURL: Api,
-  timeout: 5000,
   headers: {
     accept: "application/json",
     "Content-Type": "application/json",
